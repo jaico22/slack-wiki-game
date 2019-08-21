501317670e1e5c79bf021ac5bb175103b76cbf3f
Phillip Miller <phillip.miller@perficient.com>
2019-08-21 15:53:35 -0500

    Added more feedback; Implmeneted stats loader

--- WikiGameBot/Data/Loaders/AWSRDSInfoLoader.cs
+++ WikiGameBot/Data/Loaders/AWSRDSInfoLoader.cs
@@ -27,12 +27,9 @@ namespace WikiGameBot.Data.Loaders
             AmazonSecretsManagerConfig amazonSecretsManagerConfig = new AmazonSecretsManagerConfig();
             amazonSecretsManagerConfig.RegionEndpoint = RegionEndpoint.USEast2;
 
-            string AccessKeyID = Environment.GetEnvironmentVariable("AWS_ACCESSKEYID");
-            string SecretKey = Environment.GetEnvironmentVariable("AWS_SECRETKEY"); 
             string VersionStage = null;
 
-            IAmazonSecretsManager client = new AmazonSecretsManagerClient
-                 (AccessKeyID, SecretKey, amazonSecretsManagerConfig);
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName("us-east-2"));
 
             GetSecretValueRequest request = new GetSecretValueRequest();
             request.SecretId = secretName;
