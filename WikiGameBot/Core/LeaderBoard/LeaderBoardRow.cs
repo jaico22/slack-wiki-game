596b41d0d692e2f0ea447b4862954420251717a8
Phillip Miller <phillip.miller@perficient.com>
2019-08-22 15:36:24 -0500

    Implemented Leaderboard displays

---
+++ WikiGameBot/Core/LeaderBoard/LeaderBoardRow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WikiGameBot.Core.LeaderBoard
+{
+    public class LeaderBoardRow
+    {
+        public int Position { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int NumberOfWins { get; set; }
+
+        public int NumerOfEntries { get; set; }
+
+        public decimal WinPercentage { get; set; }
+
+    }
+}
